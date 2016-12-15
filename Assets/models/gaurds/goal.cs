8d4cb2e35f18dd6b1f9f93eba48f097ffc3c5166
Hossam-Hazem <hossamhazem94@gmail.com>
2016-12-16 00:49:30 +0200

    add gaurd model

---
+++ Assets/models/gaurds/goal.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class goal : MonoBehaviour {
+	private NavMeshAgent agent;
+	public Transform destination;
+
+	// Use this for initialization
+	void Start () {
+		agent = GetComponent<NavMeshAgent>();
+		agent.SetDestination (destination.position);
+	}
+
+	// Update is called once per frame
+	void Update () {
+	}
+}

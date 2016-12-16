a7f5417e11697ae444e8824e96fa2fc14801421a
kareem-adel <kareem.adel.fathi@gmail.com>
2016-12-16 20:01:07 +0200

    Controls |  5,6,7,8,9 abstract
    5. The player Attacks by clicking the left click
    6. The player opens item menu by pressing R
    7. The player opens weapons menu by pressing F
    8. The player chooses items/weapons from the menu by the arrows and then
    pressing
    Enter to confirm choice. Or by simply using the cursor of the mouse to
    select it.
    9. The player activates and deactivates items by pressing alt. (This
    means if the player
    presses alt it activates the item they selected from the menu. If they
    press again, it
    unselects the item.)
    10. The player can pause the game by pressing ESC or P

---
+++ Assets/playerScript.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class playerScript : MonoBehaviour {
+
+    List<GameObject> currentChosenItemFromInventory;
+
+    List<List<GameObject>> itemsInventory;
+    List<List<GameObject>> weaponsInventory;
+
+    int health = 100;
+
+	// Use this for initialization
+	void Start () {
+        itemsInventory = new List<List<GameObject>>();
+        weaponsInventory = new List<List<GameObject>>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            //attack
+        }
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            //open the item menu
+        }
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            //open the weapons menu
+        }
+        if (Input.GetKeyDown(KeyCode.AltGr))
+        {
+            //activate / deactivate object
+
+        }
+        if (Input.GetKeyDown(KeyCode.Escape) || Input.GetKeyDown(KeyCode.P))
+        {
+            //pause the game
+        }
+	}
+
+    void chooseItem(int i) {
+        currentChosenItemFromInventory = itemsInventory[i];
+    }
+
+    void chooseWeapon(int i)
+    {
+        currentChosenItemFromInventory = weaponsInventory[i];
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.CompareTag("bullet"))
+        {
+            health -= 20;
+        }
+    }
+}

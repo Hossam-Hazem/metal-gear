aff099e818df108138ebd23baf3f37733fd69724
Omar Adel <omaradel9545@gmail.com>
2016-12-17 23:53:21 +0200

    Main Menu buttons added

---
+++ Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class ButtonManager : MonoBehaviour {
+
+    public void loadScene(string selectScene)
+    {
+        Debug.Log("Load Scenes" + selectScene);
+        SceneManager.LoadScene(selectScene);
+    }
+
+    
+    public void exitButton()
+    {
+        Debug.Log("Exit");
+        Application.Quit();
+    }
+}

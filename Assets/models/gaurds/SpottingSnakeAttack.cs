1cd36ec5dd36e41197a3d550ec44ea4722479c5e
kareem-adel <kareem.adel.fathi@gmail.com>
2016-12-21 19:11:37 +0200

    Controls | basic enemy shooting when snake spotted

--- Assets/models/gaurds/SpottingSnakeAttack.cs
+++ Assets/models/gaurds/SpottingSnakeAttack.cs
@@ -1,7 +1,11 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class SpottingSnakeAttack : MonoBehaviour {
+public class SpottingSnakeAttack : MonoBehaviour
+{
+    public GameObject bullet;
+    public GameObject snake;
+    int health = 100;
 
     void Start()
     {
@@ -10,19 +14,30 @@ public class SpottingSnakeAttack : MonoBehaviour {
 
     void Update()
     {
-        RaycastHit raycastHit;
-        Vector3 origin = transform.position;
-        if (Physics.SphereCast(origin, 0.5f, transform.forward, out raycastHit, 5, 1 << 8))
-        {
-            //raycastHit.transform;
-            GameObject currentLocation = new GameObject();
-            currentLocation.transform.position = transform.position;
-            if (guardPath.destinationSnakeCurrentLocation != null)
-            {
-                Destroy(guardPath.destinationSnakeCurrentLocation.gameObject);
-            }
-            guardPath.destinationSnakeCurrentLocation = currentLocation.transform;
 
+        if (health > 0)
+        {
+            RaycastHit raycastHit;
+            Vector3 origin = transform.position;
+            if (Physics.SphereCast(origin, 0.5f, transform.forward, out raycastHit, 5, 1 << 8))
+            {
+                //transform.rotation = Quaternion.LookRotation(parentMainCamera.transform.forward);
+                transform.LookAt(snake.transform);
+                GameObject currentLocation = new GameObject();
+                currentLocation.transform.position = transform.position;
+                if (guardPath.destinationSnakeCurrentLocation != null)
+                {
+                    Destroy(guardPath.destinationSnakeCurrentLocation.gameObject);
+                }
+                guardPath.destinationSnakeCurrentLocation = currentLocation.transform;
+
+                GameObject bulletClone = (GameObject)Instantiate(bullet, bullet.transform.position, bullet.transform.rotation);
+                Rigidbody bulletCloneRigid = bulletClone.GetComponent<Rigidbody>();
+                bulletCloneRigid.isKinematic = false;
+                Vector3 dir = (snake.transform.position - this.transform.position).normalized;
+                bulletCloneRigid.velocity = dir * 50f;
+                bulletClone.SetActive(true);
+            }
         }
     }
 }

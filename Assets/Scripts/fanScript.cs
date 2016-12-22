5e848f803d1e90667b8a8c2c89d8388585d5b667
Moaaz Maamoon <moaaz.ali95@gmail.com>
2016-12-22 05:38:41 +0200

    Level one is done

---
+++ Assets/Scripts/fanScript.cs
@@ -0,0 +1,16 @@
+﻿
+using UnityEngine;
+using System.Collections;
+
+public class fanScript : MonoBehaviour {
+   public GameObject circle;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        circle.transform.Rotate(0f, 02f, 0);
+	}
+}

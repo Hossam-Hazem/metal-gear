30691f507a724d90bcb2184256bfc1ed6161cd2e
Moaaz Maamoon <moaaz.ali95@gmail.com>
2016-12-22 13:56:20 +0200

    MOAAAAAAAAAR sounds added

---
+++ Assets/Scripts/hoveringScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class hoveringScript : MonoBehaviour {
+    public AudioSource hovering;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void hover()
+    {
+        hovering.Play();
+
+    }
+}
